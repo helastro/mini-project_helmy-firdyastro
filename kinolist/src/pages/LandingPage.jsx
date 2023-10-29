822c02e36c0292c83231e557259da326be7ce697
helastro <helmyfirdy888@gmail.com>
2023-10-29 16:57:34 +0700

    landing page responsive

--- kinolist/src/pages/LandingPage.jsx
+++ kinolist/src/pages/LandingPage.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import BackgroundLandingPage from "../assets/img/background_LandingPage.jpg";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Typewriter from "typewriter-effect";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import { useLanguage } from "../LanguageContext";
+import MediaQuery from "../MediaQuery";
 
 function LandingPage() {
   const { isEnglish } = useLanguage();
+  const isMobile = MediaQuery("(max-width: 768px)");
 
   const typeEffect = (typewriter, text) => {
     typewriter.typeString(text).pauseFor(3000).deleteAll(100).pauseFor(1500).start();
@@ -16,16 +17,11 @@ function LandingPage() {
 
   return (
     <div className="overflow-x-hidden">
-      <div
-        className="bg-hero bg-center bg-cover w-screen h-screen relative"
-        style={{
-          backgroundImage: `url(${BackgroundLandingPage})`,
-        }}
-      >
+      <div className={`bg-center bg-cover w-screen h-screen relative ${isMobile ? "bg-mobile" : "bg-desktop"}`}>
         <Navbar className="bg-gradient-to-r from-black" />
         <div className="inset-0 flex flex-col items-center justify-center h-screen">
           <div className="container mx-auto relative">
-            <h1 style={{ fontFamily: "Courier New" }} className="text-9xl text-start">
+            <h1 style={{ fontFamily: "Courier New" }} className={` ${isMobile ? "text-center text-6xl" : "text-9xl text-start"}`}>
               <Typewriter
                 options={{ loop: true }}
                 onInit={(typewriter) => {
@@ -33,10 +29,12 @@ function LandingPage() {
                 }}
               />
             </h1>
-            <p className="text-xl mb-6">{isEnglish ? "Organize, Rate, Get Recommendations." : "Atur, Nilai, Dapatkan Rekomendasi."}</p>
-            <Link to="/How-to-Use">
-              <Button label={isEnglish ? "Learn More" : "Pelajari Lebih Lanjut"} />
-            </Link>
+            <p className={`${isMobile ? "text-center mb-6 text-xs" : "text-xl mb-6"}`}>{isEnglish ? "Organize, Rate, Get Recommendations." : "Atur, Nilai, Dapatkan Rekomendasi."}</p>
+            <div className={isMobile ? "flex justify-center items-center" : ""}>
+              <Link to="/How-to-Use">
+                <Button label={isEnglish ? "Learn More" : "Pelajari Lebih Lanjut"} />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
