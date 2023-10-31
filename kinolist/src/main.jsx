07674e27a89a3d763ee997364d5a8136924f7aeb
helastro <helmyfirdy888@gmail.com>
2023-10-31 14:00:59 +0700

    fix language context

--- kinolist/src/main.jsx
+++ kinolist/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { LanguageProvider } from "./LanguageContext.jsx";
+import { LanguageProvider } from "./config/LanguageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
